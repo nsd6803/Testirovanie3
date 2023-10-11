3b28a6f099abe1789db06e394e76240549c41ad7
tiklkun <tiklkun4869@gmail.com>
2023-10-11 15:59:32 +0300

    TDD implementation

--- OldRussianConverter.py
+++ OldRussianConverter.py
@@ -2,7 +2,51 @@ import unittest
 
 
 class OldRussianConverter:
-    pass
+    ARSHIN_TO_INCH = 28.0  # Conversion factor
+    ARSHIN_TO_METER = 0.7112
+    ARSHIN_TO_SHAKU = 0.71
+    VEDRO_TO_SHO = 40.0
+    VEDRO_TO_BARREL = 0.36
+    VEDRO_TO_LITER = 12.3
+    BATMAN_TO_KG = 16.38
+    BATMAN_TO_KIN = 1.25
+    BATMAN_TO_POUND = 36.11
+
+    @classmethod
+    def arshin_to_inch(cls, value):
+        return value * cls.ARSHIN_TO_INCH
+
+    @classmethod
+    def arshin_to_meter(cls, value):
+        return value * cls.ARSHIN_TO_METER
+
+    @classmethod
+    def arshin_to_shaku(cls, value):
+        return value * cls.ARSHIN_TO_SHAKU
+
+    @classmethod
+    def vedro_to_sho(cls, value):
+        return value * cls.VEDRO_TO_SHO
+
+    @classmethod
+    def vedro_to_barrel(cls, value):
+        return value * cls.VEDRO_TO_BARREL
+
+    @classmethod
+    def vedro_to_liter(cls, value):
+        return value * cls.VEDRO_TO_LITER
+
+    @classmethod
+    def batman_to_kg(cls, value):
+        return value * cls.BATMAN_TO_KG
+
+    @classmethod
+    def batman_to_kin(cls, value):
+        return value * cls.BATMAN_TO_KIN
+
+    @classmethod
+    def batman_to_pound(cls, value):
+        return value * cls.BATMAN_TO_POUND
 
 
 class TestOldRussianConverter(unittest.TestCase):
