aab4e764db99f8db07c765e74ef0eed7ff4c69d4
nsd6803 <nsd6803@mail.ru>
2023-10-11 17:57:59 +0300

    TDD_JapaneseConverter

--- features/AmericanConverter.feature
+++ features/AmericanConverter.feature
@@ -1,46 +1,46 @@
 Feature: American Converter
 
-  Scenario: Convert Inch to Arshin
-    Given the value is 10 Inch
-    When I convert it to Arshin
-    Then the result should be 0.4 Arshin
+  Scenario: Convert Inch_A to Arshin_A
+    Given the value is 10 Inch_A
+    When I convert it to Arshin_A
+    Then the result should be 0.4 Arshin_A
 
-  Scenario: Convert Inch to Meter
-    Given the value is 100 Inch
-    When I convert it to Meter
-    Then the result should be 2.5 Meter
+  Scenario: Convert Inch_A to Meter_A
+    Given the value is 100 Inch_A
+    When I convert it to Meter_A
+    Then the result should be 2.5 Meter_A
 
-  Scenario: Convert Inch to Syaku
-    Given the value is 100 Inch
-    When I convert it to Syaku
-    Then the result should be 8.4 Shaku
+  Scenario: Convert Inch_A to Syaku_A
+    Given the value is 100 Inch_A
+    When I convert it to Syaku_A
+    Then the result should be 8.4 Syaku_A
 
-  Scenario: Convert Barrel to Ce
-    Given the value is 100 Barrel
-    When I convert it to Ce
-    Then the result should be 21502.5 Ce
+  Scenario: Convert Barrel_A to Ce_A
+    Given the value is 100 Barrel_A
+    When I convert it to Ce_A
+    Then the result should be 21502.5 Ce_A
 
-  Scenario: Convert Barrel to Vedro
-    Given the value is 100 Barrel
-    When I convert it to Vedro
-    Then the result should be 245003.2 Veddro
+  Scenario: Convert Barrel_A to Vedro_A
+    Given the value is 100 Barrel_A
+    When I convert it to Vedro_A
+    Then the result should be 245004.2 Vedro_A
 
-  Scenario: Convert Barrel to Liter
-    Given the value is 10 Barrel
-    When I convert it to Liter
-    Then the result should be 1192 Liter
+  Scenario: Convert Barrel_A to Liter_A
+    Given the value is 10 Barrel_A
+    When I convert it to Liter_A
+    Then the result should be 1192 Liter_A
 
-  Scenario: Convert Pound to Kilogram
-    Given the value is 100 Pound
-    When I convert it to Kilogram
-    Then the result should be 45.4 Kilogram
+  Scenario: Convert Pound_A to Kilogram_A
+    Given the value is 100 Pound_A
+    When I convert it to Kilogram_A
+    Then the result should be 45.4 Kilogram_A
 
-  Scenario: Convert Pound to Kin
-    Given the value is 100 Pound
-    When I convert it to Kin
-    Then the result should be 27.2 Kin
+  Scenario: Convert Pound_A to Kin_A
+    Given the value is 100 Pound_A
+    When I convert it to Kin_A
+    Then the result should be 27.2 Kin_A
 
-  Scenario: Convert Pound to Batman
-    Given the value is 100 Pound
-    When I convert it to Batman
-    Then the result should be 185.7 Pound
+  Scenario: Convert Pound_A to Batman_A
+    Given the value is 100 Pound_A
+    When I convert it to Batman_A
+    Then the result should be 185.7 Batman_A
--- features/steps/AmericanConverter_steps.py
+++ features/steps/AmericanConverter_steps.py
@@ -1,115 +1,115 @@
 from behave import given, when, then
+from AmericanConverter import AmericanConverter
 
-
-@given('the value is {value} Inch')
+@given('the value is {value} Inch_A')
 def set_inch_value(context, value):
     context.value = float(value)
 
 
-@when('I convert it to Arshin')
+@when('I convert it to Arshin_A')
 def convert_to_arshin(context):
     context.result = AmericanConverter.inch_to_arshin(context.value)
 
 
-@then('the result should be {expected} Arshin')
+@then('the result should be {expected} Arshin_A')
 def verify_arshin_result(context, expected):
     expected = float(expected)
     assert context.result == expected
 
 
-@when('I convert it to Meter')
+@when('I convert it to Meter_A')
 def convert_to_meter(context):
     context.result = AmericanConverter.inch_to_meter(context.value)
 
 
-@then('the result should be {expected} Meter')
+@then('the result should be {expected} Meter_A')
 def verify_meter_result(context, expected):
     expected = float(expected)
     assert context.result == expected
 
 
-@when('I convert it to Syaku')
+@when('I convert it to Syaku_A')
 def convert_to_syaku(context):
     context.result = AmericanConverter.inch_to_syaku(context.value)
 
 
-@then('the result should be {expected} Syaku')
+@then('the result should be {expected} Syaku_A')
 def verify_syaku_result(context, expected):
     expected = float(expected)
     assert context.result == expected
 
 
-@given('the value is {value} Barrel')
+@given('the value is {value} Barrel_A')
 def set_barrel_value(context, value):
     context.value = float(value)
 
 
-@when('I convert it to Ce')
+@when('I convert it to Ce_A')
 def convert_to_ce(context):
     context.result = AmericanConverter.barrel_to_ce(context.value)
 
 
-@then('the result should be {expected} Ce')
+@then('the result should be {expected} Ce_A')
 def verify_ce_result(context, expected):
     expected = float(expected)
     assert context.result == expected
 
 
-@when('I convert it to Vedro')
+@when('I convert it to Vedro_A')
 def convert_to_vedro(context):
     context.result = AmericanConverter.barrel_to_vedro(context.value)
 
 
-@then('the result should be {expected} Vedro')
+@then('the result should be {expected} Vedro_A')
 def verify_vedro_result(context, expected):
     expected = float(expected)
     assert context.result == expected
 
 
-@when('I convert it to Liter')
+@when('I convert it to Liter_A')
 def convert_to_liter(context):
     context.result = AmericanConverter.barrel_to_liter(context.value)
 
 
-@then('the result should be {expected} Liter')
+@then('the result should be {expected} Liter_A')
 def verify_liter_result(context, expected):
     expected = float(expected)
     assert context.result == expected
 
 
-@given('the value is {value} Pound')
+@given('the value is {value} Pound_A')
 def set_pound_value(context, value):
     context.value = float(value)
 
 
-@when('I convert it to Kilogram')
+@when('I convert it to Kilogram_A')
 def convert_to_kilogram(context):
     context.result = AmericanConverter.pound_to_kilogram(context.value)
 
 
-@then('the result should be {expected} Kilogram')
+@then('the result should be {expected} Kilogram_A')
 def verify_kilogram_result(context, expected):
     expected = float(expected)
     assert context.result == expected
 
 
-@when('I convert it to Kin')
+@when('I convert it to Kin_A')
 def convert_to_kin(context):
     context.result = AmericanConverter.pound_to_kin(context.value)
 
 
-@then('the result should be {expected} Kin')
+@then('the result should be {expected} Kin_A')
 def verify_kin_result(context, expected):
     expected = float(expected)
     assert context.result == expected
 
 
-@when('I convert it to Batman')
+@when('I convert it to Batman_A')
 def convert_to_batman(context):
     context.result = AmericanConverter.pound_to_batman(context.value)
 
 
-@then('the result should be {expected} Batman')
+@then('the result should be {expected} Batman_A')
 def verify_batman_result(context, expected):
     expected = float(expected)
     assert context.result == expected
